17dcbe1e14d882641dd21205b932931c64e348d0
hanjiho <hanjiho97@gist.ac.kr>
2023-03-14 01:58:31 +0900

    update run_and_generate_report.py to parse error data

--- run_and_generate_report.py
+++ run_and_generate_report.py
@@ -9,7 +9,7 @@ import glob
 import openpyxl
 
 #setting logger
-logger = logging.getLogger('make_result_files.py')
+logger = logging.getLogger('run_and_generate_report')
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
@@ -19,7 +19,7 @@ logger.addHandler(stream_handler)
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='make_result_file')
+    parser = argparse.ArgumentParser(description='run_and_generate_report')
     parser.add_argument('--dataset_path', type=str, required=True, help='Path to KITTI dataset')
     parser.add_argument('--output_dir', type=str, required=True, help='Path to output directory')
     args = parser.parse_args()
@@ -30,7 +30,7 @@ def parse_args():
     return args
 
 
-def createFolder(directory):
+def create_folder(directory):
     try:
         if not os.path.exists(directory):
             os.makedirs(directory)
@@ -39,10 +39,10 @@ def createFolder(directory):
         sys.exit(1)
 
 
-def run_rtapmap_all_features(output_dir, feature_num):
-    for index in range(feature_num):
+def run_rtapmap_all_features(output_dir, dataset_path):
+    for index in range(16):
         logger.info('{}th feature start!'.format(index))
-        createFolder(output_dir + str(index))
+        create_folder(output_dir + str(index))
         try:
             output_text = subprocess.check_output([
                 '/root/rtabmap_install/bin/rtabmap-kitti_dataset',
@@ -52,16 +52,16 @@ def run_rtapmap_all_features(output_dir, feature_num):
                 '--RGBD/LinearUpdate', '0',
                 '--GFTT/QualityLevel', '0.01',
                 '--GFTT/MinDistance', '7',
-                '--SuperPoint/ModelPath', '/root/Documents/RTAB-Map/superpoint.pt',
+                '--SuperPoint/ModelPath', dataset_path+'superpoint.pt',
                 '--OdomF2M/MaxSize', '3000',
                 '--Mem/STMSize', '30',
                 '--Kp/MaxFeatures', '750',
-                '--Kp/DetectorStrategy', '{}'.format(index),
+                '--Kp/DetectorStrategy', str(index),
                 '--Vis/MaxFeatures', '1500',
-                '--Vis/FeatureType', '{}'.format(index),
+                '--Vis/FeatureType', str(index),
                 '--output', output_dir+str(index),
-                '--gt', '/root/Documents/RTAB-Map/data_odometry_poses/poses/07.txt',
-                '/root/Documents/RTAB-Map/data_odometry_gray/dataset/sequences/07'],
+                '--gt', dataset_path+'data_odometry_poses/poses/07.txt',
+                dataset_path+'data_odometry_gray/dataset/sequences/07'],
                 encoding='utf-8')
         except subprocess.CalledProcessError:
             logger.error('Fail to excute {}th feature.'.format(index))
@@ -73,89 +73,100 @@ def run_rtapmap_all_features(output_dir, feature_num):
         logger.info('Save complete!')
 
 
-def parse_time_data(output_dir):
+def parse_time_error_data(output_dir):
     logger.info('Parsing time data')
     time_dataset = {}
+    error_dataset = {}
     feature_name = ['SURF', 'SIFT', 'ORB', 'FAST/FREAK',
                     'FAST/BRIEF', 'GFTT/FREAK', 'GFTT/BRIEF', 'BRISK',
                     'GFTT/ORB', 'KAZE', 'ORB-OCTREE', 'SuperPoint',
                     'SURF/FREAK', 'GFTT/DAISY', 'SURF/DAISY', 'PyDetector']
     file_path_list = glob.glob(output_dir+'*.txt')
     for file_path in file_path_list:
+        index = int(file_path.split('.')[0].split('/')[-1])
         time_data = {'camera':{}, 'odom':{}, 'slam':{}}
 
         with open(file_path, 'r') as file:
             lines = file.readlines()
 
-        carmera_time = []
+        camera_time = []
         odom_time = []
         slam_time = []
         for line in lines:
-            logger.info(line)
             words = line.split()
-            if words and words[0] == 'Iteration':
-                carmera_time.append(int(words[3].split('=')[-1][:-3]))
+            if not words:
+                continue
+            if words[0][:-1] == 'translational_rmse':
+                error_dataset[feature_name[index]]['translational_rmse'] = float(words[-2])
+            elif words[0][:-1] == 'rotational_rmse':
+                error_dataset[feature_name[index]]['rotational_rmse'] = float(words[-2])
+            elif words[0] == 'Iteration':
+                camera_time.append(int(words[3].split('=')[-1][:-3]))
                 odom_time.append(int(words[5].split('=')[-1][:-3]))
                 slam_time.append(int(words[6].split('=')[-1][:-3]))
+        try:
+            time_data['camera']['average'] = sum(camera_time) / len(camera_time)
+            time_data['camera']['median'] = sorted(camera_time)[len(camera_time)//2]
+            time_data['camera']['max'] = max(camera_time)
+            time_data['camera']['min'] = min(camera_time)
 
-        if len(carmera_time) == 0:
-            time_data['camera']['average'] = 0
-            time_data['camera']['median'] = 0
-            time_data['camera']['max'] = 0
-            time_data['camera']['min'] = 0
-        else:
-            time_data['camera']['average'] = sum(carmera_time) / len(carmera_time)
-            time_data['camera']['median'] = sorted(carmera_time)[len(carmera_time)//2]
-            time_data['camera']['max'] = max(carmera_time)
-            time_data['camera']['min'] = min(carmera_time)
-
-        if len(odom_time) == 0:
-            time_data['odom']['average'] = 0
-            time_data['odom']['median'] = 0
-            time_data['odom']['max'] = 0
-            time_data['odom']['min'] = 0
-        else:
             time_data['odom']['average'] = sum(odom_time) / len(odom_time)
             time_data['odom']['median'] = sorted(odom_time)[len(odom_time)//2]
             time_data['odom']['max'] = max(odom_time)
             time_data['odom']['min'] = min(odom_time)
 
-        if len(slam_time) == 0:
-            time_data['slam']['average'] = 0
-            time_data['slam']['median'] = 0
-            time_data['slam']['max'] = 0
-            time_data['slam']['min'] = 0
-        else:
             time_data['slam']['average'] = sum(slam_time) / len(slam_time)
             time_data['slam']['median'] = sorted(slam_time)[len(slam_time)//2]
             time_data['slam']['max'] = max(slam_time)
             time_data['slam']['min'] = min(slam_time)
+        except ZeroDivisionError:
+            logger.error('Cannot divide into zero! Check gt path!')
+            sys.exit(1)
+        except IndexError:
+            logger.error('Cannot access to wrong index!')
+            sys.exit(1)
 
-        index = int(file_path.split('.')[0].split('/')[-1])
+        if not camera_time or not odom_time or not slam_time:
+            logger.warning('Cannot parse time data from ouput! Check command!')
         time_dataset[feature_name[index]] = time_data
     logger.info('Parsing done')
-    return time_dataset
+    return time_dataset, error_dataset
 
 
-def write_excel(time_dataset, output_dir):
+def write_excel(time_dataset, error_dataset, output_dir):
     logger.info('Trying to write data in excel format')
-    workbook = openpyxl.load_workbook('/root/slam_team2/time_data_format.xlsx', data_only=True)
+    workbook = openpyxl.load_workbook('/root/slam_team2/data_format.xlsx', data_only=True)
     worksheet = workbook.active
-    row = 2
-    for name, time_data in time_dataset.items():
-        worksheet.cell(row, 1, name)
-        for index, (type, data) in enumerate(time_data.items()):
-            worksheet.cell(row+index, 2, type)
+    #write time dataset in a row
+    for row, (name, time_data) in enumerate(time_dataset.items()):
+        for type, data in time_data.items():
+            if type == 'camera':
+                index = 0
+            elif type == 'odom':
+                index =17
+            elif type == 'slam':
+                index = 34
+            worksheet.cell(row+index+2, 2, name)
+            worksheet.cell(row+index+2, 1, type)
             for i, value in enumerate(data.values()):
-                worksheet.cell(row+index, 3+i, value)
-        row += 3
-    workbook.save(output_dir+'report/time_result.xlsx')
-    logger.info('Finsh!')
+                worksheet.cell(row+index+2, 3+i, value)
+    logger.info('Writing time data done!')
+    #write error dataset in a row
+    for row, (name, error_data) in enumerate(error_dataset.items()):
+        worksheet.cell(row+2, 8, name)
+        for type, data in error_data.items():
+            if type == 'translational_rmse':
+                worksheet.cell(row+2, 9, data)
+            elif type == 'rotational_rmse':
+                worksheet.cell(row+2, 10, data)
+    logger.info('Writing error data done!')
+    workbook.save(output_dir+'report/result.xlsx')
+    logger.info('Saving '+output_dir+'report/result.xlsx complete!')
 
 
 if __name__ == '__main__':
     args = parse_args()
-    createFolder(args.output_dir)
-    run_rtapmap_all_features(args.output_dir, 16)
-    time_dataset = parse_time_data(args.output_dir)
-    write_excel(time_dataset, args.output_dir)
+    create_folder(args.output_dir)
+    run_rtapmap_all_features(args.output_dir, args.dataset_path)
+    time_dataset, error_dataset = parse_time_error_data(args.output_dir)
+    write_excel(time_dataset, error_dataset, args.output_dir)
